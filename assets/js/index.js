63271c3c2da5e28f250eea66a28d2b3b82d9176a
pzwt4cc <dotsenkoviktor8t@gmail.com>
2026-09-24 13:33:55 +0300

    Fix: print number format

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@ function formattedPhone(phone) {
   const part2 = cleanPhone.slice(6, 8);
   const part3 = cleanPhone.slice(8, 10);
 
-  return `+380 (${code} ${part1}-${part2}-${part3})`;
+  return `+38 (${code} ${part1}-${part2}-${part3})`;
 }
 
 console.log(formattedPhone("0671234567"));
